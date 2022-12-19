1af862b3e799d6043aa3eb1703964bcbacce53b9
Radwa Eid <radwaeid4@gmail.com>
2022-12-19 11:39:07 +0200

    Added authentication and migration

--- database/migrations/2022_12_19_091623_create_posts_table.php
+++ database/migrations/2022_12_19_091623_create_posts_table.php
@@ -13,14 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('role');
-            $table->rememberToken();
+            $table->text('content');
+            $table->integer('user_id');
             $table->timestamps();
         });
     }
@@ -32,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('posts');
     }
 };
